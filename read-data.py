1883d49e59672e67a20f98be66092a3a63e6a9c2
Anthony DePaul <ad5146@cs.ship.edu>
2019-04-24 11:16:52 -0400

    reduced dataframes to just portland

--- read-data.py
+++ read-data.py
@@ -3,26 +3,34 @@ import tarfile
 import pandas as pd
 import matplotlib.pyplot as plt
 
-PATH = "/home/jb7656/MachineLearning/Group Project/Datasets/historical-hourly-weather-data/"
+PATH = "/media/ad5146/Anthony D/school/2018-19/spring/csc 403 (ML)/CSC403_FinalProject/historical-hourly-weather-data/"
 FILE_NAMES = {"humidity.csv", "pressure.csv", "temperature.csv",
               "weather_description.csv", "wind_direction.csv", "wind_speed.csv"}
+
+# reads in all csv's of data
 def load_weather_data():
-    #need to read in all csv's of data
-    #return pd.read_csv(PATH + FILE_NAME)
-    csv_list = []
+    result_df = pd.DataFrame()
+    
     for x in FILE_NAMES:
-        csv_list.append(pd.read_csv(PATH + x))
-        print(x, "was read in successfully")
-    return csv_list 
         
+        df = pd.read_csv(PATH + x)
+        
+        df = df[["Portland"]]
+        
+        result_df = pd.concat([result_df, df])
+	
+        print(x, "was read in successfully")
+        print(result_df)
+        
+    return result_df
 
 def main():
     data = load_weather_data()
+    print("data")
+    print(data)
+    
     portland = data[2]
     portland_df = pd.DataFrame(portland)
     print(portland_df)
-    
-
-    
 
 main()
