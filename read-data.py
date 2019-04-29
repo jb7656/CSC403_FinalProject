dee0579e9dac45e1d1e4452b6129e768142e65e8
Anthony DePaul <ad5146@cs.ship.edu>
2019-04-29 11:49:57 -0400

    fixed column labels

--- read-data.py
+++ read-data.py
@@ -3,9 +3,10 @@ import tarfile
 import pandas as pd
 import matplotlib.pyplot as plt
 
-PATH = "/home/jb7656/MachineLearning/Group Project/Datasets/historical-hourly-weather-data/"
+PATH = os.getcwd() + "/data/" # "/home/jb7656/MachineLearning/Group Project/Datasets/historical-hourly-weather-data/"
 FILE_NAMES = {"humidity.csv", "pressure.csv", "temperature.csv",
 "weather_description.csv", "wind_direction.csv", "wind_speed.csv"}
+
 # reads in all csv's of data
 def load_weather_data():
     result_df = pd.DataFrame()
@@ -17,22 +18,26 @@ def load_weather_data():
 
         if(index == 0):
             result_df = pd.concat([result_df, df])
+            result_df.columns = [x]
             
         else:
             result_df.insert(index, x, df, allow_duplicates = False)
 
         index = index + 1
         print(x, "was read in successfully")
-        print(result_df)   
+        print(result_df)
     return result_df
 
 def main():
     data = load_weather_data()
     print("data")
-    data.columns = ['weather_description', 'humidity', 'pressure', 'wind_speed',
-       'temperature', 'wind_direction']
+    # data.columns = ['weather_description', 'humidity', 'pressure', 'wind_speed',
+    #    'temperature', 'wind_direction']
     print(data)
     print(data.columns)
+    print(data.values[1])
+    print(data.values[2])
+    print(data.values[3])
     
     #portland = data[2]
     #portland_df = pd.DataFrame(portland)
